32fa03143833121c768cde286abc0473085c3864
Aleksey Korshuk <48794610+AlekseyKorshuk@users.noreply.github.com>
2023-05-11 13:52:24 -0700

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+wget https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/i/3EHBfJGmiv--MA -O data/genome_scores.csv
+wget https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/i/Go-JBXBqKtk6Vg -O data/genome_tags.csv
+wget https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/i/QPo2T-LjytuHMg -O data/movie.csv
+wget https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/i/sFD-ql1stXijKg -O data/rating.csv
+wget https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/i/1ZVz6Jq5RzThaQ -O data/tag.csv
